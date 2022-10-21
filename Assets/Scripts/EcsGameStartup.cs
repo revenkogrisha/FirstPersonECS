1aec907ab3642806c8cc7614e78bead994581ec8
revenkogrisha <grisha@revenko.org>
2022-10-21 19:59:36 +0300

    Added ECS initialization

---
+++ Assets/Scripts/EcsGameStartup.cs
@@ -0,0 +1,40 @@
+using Leopotam.Ecs;
+using UnityEngine;
+using Voody.UniLeo;
+
+public sealed class EcsGameStartup : MonoBehaviour
+{
+    private EcsWorld _world;
+    private EcsSystems _systems;
+
+    private void Start()
+    {
+        // Инициализация классов
+        _world = new EcsWorld();
+        _systems = new EcsSystems(_world);
+
+        // Конвертация для работы UniLeo
+        _systems.ConvertScene();
+
+        // Инициализация всех систем
+        _systems.Init();
+    }
+
+    private void Update()
+    {
+        // Запускает все IEcsRunSystem системы
+        _systems.Run();
+    }
+
+    private void OnDestroy()
+    {
+        // Clears all data
+        if (_systems == null)
+            return;
+
+        _systems.Destroy();
+        _systems = null;
+        _world.Destroy();
+        _world = null;
+    }
+}
