cea79e0bad6adf1352309e016168c067674ee0f8
revenkogrisha <grisha@revenko.org>
2022-10-29 23:50:37 +0300

    Added jump logic

---
+++ Assets/Scripts/Ecs/Player/Components/JumpComponent.cs
@@ -0,0 +1,7 @@
+using System;
+
+[Serializable]
+public struct JumpComponent
+{
+    public float Force;
+}
---
+++ Assets/Scripts/Ecs/Player/Systems/PlayerJumpSystem.cs
@@ -0,0 +1,28 @@
+using Leopotam.Ecs;
+using UnityEngine;
+
+public class PlayerJumpSystem : IEcsRunSystem
+{
+    private readonly EcsFilter<PlayerTag, GroundCheckComponent, JumpComponent, GravityComponent, JumpEvent> _filter = null;
+
+    public void Run()
+    {
+        foreach (var i in _filter)
+        {
+            ref var entity = ref _filter.GetEntity(i);
+            ref var moveableComponent = ref entity.Get<MoveableComponent>();
+            ref var groundCheckComponent = ref _filter.Get2(i);
+            ref var jumpComponent = ref _filter.Get3(i);
+            ref var gravityComponent = ref _filter.Get4(i);
+
+            ref var velocity = ref moveableComponent.Velocity;
+            ref var force = ref jumpComponent.Force;
+            ref var gravity = ref gravityComponent.Gravity.y;
+
+            if (!groundCheckComponent.IsGrounded)
+                continue;
+
+            velocity.y = Mathf.Sqrt(force * -2 * gravity);
+        }
+    }
+}
